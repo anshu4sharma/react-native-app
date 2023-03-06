ca232377a12e68141e274cb0b1df48dbad9cf3d7
anshu0x <anshusharma6327@gmail.com>
2023-03-06 10:01:39 +0530

    React Native tutorial in Hindi #16 How to make Grid

--- App.tsx
+++ App.tsx
@@ -23,10 +23,16 @@ const App = () => {
         data={users}
         renderItem={({item}) => <Text style={styles.text}>{item.name}</Text>}
       />
-      <ScrollView style={styles.div}>
-        {[...Array(100)].map((_, index) => {
-          return <Text>{index}</Text>;
-        })}
+      <ScrollView>
+        <View style={styles.grid}>
+          {[...Array(100)].map((_, index) => {
+            return (
+              <Text key={index} style={styles.div}>
+                {index}
+              </Text>
+            );
+          })}
+        </View>
       </ScrollView>
     </View>
   );
@@ -41,8 +47,18 @@ const styles = StyleSheet.create({
     fontSize: 40,
   },
   div: {
-    display: 'flex',
-    gap: 10,
     textAlign: 'center',
+    borderColor: 'red',
+    borderWidth: 1,
+    height: 100,
+    width: 100,
+    textAlignVertical: 'center',
+    margin: 2,
+    // padding: 4,
+  },
+  grid: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    flex: 1,
   },
 });

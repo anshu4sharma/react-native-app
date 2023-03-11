ffe968dfb8e1fe5e7276330fab50f3b548559474
anshu0x <anshusharma6327@gmail.com>
2023-03-11 14:44:04 +0530

    icon issue fixed

--- App.tsx
+++ App.tsx
@@ -1,46 +1,52 @@
-import React, {useState} from 'react';
-import {View, Button, Linking, Text} from 'react-native';
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const App = () => {
-  const [result, SetResult] = useState('');
-  const [error, seterror] = useState('');
-  const LinkOpen = async payApp => {
-    let url = '';
-    switch (payApp) {
-      case 'PAYTM':
-        url = 'paytmmp://';
-        break;
-      case 'GPAY':
-        url = 'tez://upi/';
-        break;
-      case 'PHONEPE':
-        url = 'phonepe://';
-        break;
-    }
-    url =
-      url + 'pay?pa=9518008087@paytm&pn=Anshu&am=1000&cu=INR&tn=Test%20payment';
-    try {
-      const res = await Linking.openURL(url);
-      console.warn(res);
-      SetResult(res);
-    } catch (error) {
-      seterror(error);
-      console.warn(error);
-    }
-  };
+function HomeScreen() {
   return (
-    <View className="flex-1 justify-evenly items-center">
-      <Button onPress={()=>LinkOpen("PAYTM")} title="OPen PAYTM Apps" />
-      <Button onPress={()=>LinkOpen("PHONEPE")} title="OPen PHONEPE Apps" />
-      <Button onPress={()=>LinkOpen("GPAY")} title="OPen GPAY Apps" />
-      {result && (
-        <Text className="text-red-600">Result : {JSON.stringify(result)}</Text>
-      )}
-      {error && (
-        <Text className="text-red-600">Error : {JSON.stringify(error)}</Text>
-      )}
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text className="text-red-600">Home!</Text>
     </View>
   );
-};
+}
 
-export default App;
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName: string;
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+            // @ts-ignore
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}

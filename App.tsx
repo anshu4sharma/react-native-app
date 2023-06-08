bb27ec1d4810d98c3521c694c129a316662c9749
anshu0x <anshusharma6327@gmail.com>
2023-06-08 13:46:59 +0530

    keyboard scroll view added :

--- App.tsx
+++ App.tsx
@@ -1,129 +1,122 @@
-// How to Access Call Logs of Android Devices from React Native App
-// https://aboutreact.com/access-call-logs-of-android-devices/
-
-// import React in our code
-import React, {useState, useEffect} from 'react';
-
-// import all the components we are going to use
 import {
-  SafeAreaView,
-  Platform,
-  StyleSheet,
-  Text,
   View,
-  PermissionsAndroid,
-  FlatList,
+  Text,
+  ScrollView,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
 } from 'react-native';
-
-// import CallLogs API
-import CallLogs from 'react-native-call-log';
+import React from 'react';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const App = () => {
-  const [listData, setListDate] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      if (Platform.OS != 'ios') {
-        try {
-          //Ask for runtime permission
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.READ_CALL_LOG,
-            {
-              title: 'Call Log Example',
-              message: 'Access your call logs',
-              buttonNeutral: 'Ask Me Later',
-              buttonNegative: 'Cancel',
-              buttonPositive: 'OK',
-            },
-          );
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            CallLogs.loadAll().then(c => setListDate(c));
-            CallLogs.load(3).then(c => console.log(c));
-          } else {
-            console.log('Call Log permission denied');
-          }
-        } catch (e) {
-          console.log(e);
-        }
-      } else {
-        console.log('Sorry! You can’t get call logs in iOS devices');
-      }
-    }
-    fetchData();
-  }, []);
-
-  const ItemView = ({item}) => {
-    return (
-      // FlatList Item
-      <View>
-        <Text style={styles.textStyle}>
-          Name : {item.name ? item.name : 'NA'}
-          {'\n'}
-          DateTime : {item.dateTime}
-          {'\n'}
-          Duration : {item.duration}
-          {'\n'}
-          PhoneNumber : {item.phoneNumber}
-          {'\n'}
-          RawType : {item.rawType}
-          {'\n'}
-          Timestamp : {item.timestamp}
-          {'\n'}
-          Type : {item.type}
-        </Text>
-      </View>
-    );
-  };
-
-  const ItemSeparatorView = () => {
-    return (
-      // FlatList Item Separator
-      <View
-        style={{
-          height: 0.5,
-          width: '100%',
-          backgroundColor: '#C8C8C8',
-        }}
-      />
-    );
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.titleText}>
-          How to Access Call Logs of Android Devices from React Native App
-        </Text>
-        <FlatList
-          data={listData}
-          //data defined in constructor
-          ItemSeparatorComponent={ItemSeparatorView}
-          //Item Separator View
-          renderItem={ItemView}
-          keyExtractor={(item, index) => index.toString()}
+    <KeyboardAwareScrollView
+      enableAutomaticScroll={true}
+      extraScrollHeight={100}
+      scrollEnabled={true}
+      enableOnAndroid={true}>
+      <ScrollView>
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            margin: 20,
+            padding: 10,
+          }}
+          autoFocus={true}
         />
-      </View>
-    </SafeAreaView>
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            margin: 20,
+            padding: 10,
+            marginVertical: 10,
+          }}
+        />
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            margin: 20,
+            marginVertical: 10,
+            padding: 10,
+          }}
+        />
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            marginVertical: 10,
+            margin: 20,
+            padding: 10,
+          }}
+        />
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            margin: 20,
+            marginVertical: 10,
+            padding: 10,
+          }}
+        />
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            marginVertical: 10,
+            margin: 20,
+            padding: 10,
+          }}
+        />
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            marginVertical: 10,
+            margin: 20,
+            padding: 10,
+          }}
+        />
+        <TextInput
+          placeholder="Enter your name"
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            marginVertical: 10,
+            margin: 20,
+            padding: 10,
+          }}
+        />
+
+        <TouchableOpacity
+          style={{
+            backgroundColor: 'blue',
+            padding: 10,
+            margin: 20,
+            alignItems: 'center',
+            marginVertical: 10,
+            borderRadius: 10,
+          }}>
+          <Text
+            style={{
+              color: 'white',
+            }}>
+            Submit
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardAwareScrollView>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 10,
-  },
-  titleText: {
-    fontSize: 22,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: 'black',
-  },
-  textStyle: {
-    fontSize: 16,
-    color: 'black',
-    marginVertical: 10,
-  },
-});
